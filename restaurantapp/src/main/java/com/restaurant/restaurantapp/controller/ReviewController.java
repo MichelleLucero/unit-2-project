24248ca4af7ca625bcfefcb46851f12f81adf774
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-06 21:30:22 -0500

    created createRestaurantReview. ML + JW

--- restaurantapp/src/main/java/com/restaurant/restaurantapp/controller/ReviewController.java
+++ restaurantapp/src/main/java/com/restaurant/restaurantapp/controller/ReviewController.java
@@ -4,10 +4,7 @@ import com.restaurant.restaurantapp.model.Restaurant;
 import com.restaurant.restaurantapp.model.Review;
 import com.restaurant.restaurantapp.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -24,8 +21,15 @@ public class ReviewController {
 
     //    http://localhost:9092/api/restaurants/1/reviews
     @GetMapping("/restaurants/{restaurantId}/reviews")
-    public List<Review> getRestaurantReviews(@PathVariable(value = "restaurantId") Long categoryId){
+    public List<Review> getRestaurantReviews(@PathVariable(value = "restaurantId") Long restaurantId){
         LOGGER.info("calling getRestaurantReviews from controller");
-        return reviewService.getRestaurantReviews(categoryId);
+        return reviewService.getRestaurantReviews(restaurantId);
+    }
+
+    //    http://localhost:9092/api/restaurants/1/reviews
+    @PostMapping("/restaurants/{restaurantId}/reviews/{userId}")
+    public Review createRestaurantReview(@PathVariable(value = "restaurantId") Long restaurantId, @PathVariable(value = "restaurantId") Long userId, @RequestBody Review reviewObject){
+        LOGGER.info("calling createRestaurantReview from controller");
+        return reviewService.createRestaurantReview(restaurantId, userId,reviewObject);
     }
 }
--- restaurantapp/src/main/java/com/restaurant/restaurantapp/service/ReviewService.java
+++ restaurantapp/src/main/java/com/restaurant/restaurantapp/service/ReviewService.java
@@ -1,18 +1,27 @@
 package com.restaurant.restaurantapp.service;
 
+import com.restaurant.restaurantapp.exception.InformationNotFoundException;
+import com.restaurant.restaurantapp.model.Category;
+import com.restaurant.restaurantapp.model.Restaurant;
 import com.restaurant.restaurantapp.model.Review;
+import com.restaurant.restaurantapp.model.User;
+import com.restaurant.restaurantapp.repository.RestaurantRepository;
 import com.restaurant.restaurantapp.repository.ReviewRepository;
 import com.restaurant.restaurantapp.repository.ReviewRepository;
+import com.restaurant.restaurantapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
 public class ReviewService {
     private ReviewRepository reviewRepository;
+    private RestaurantRepository restaurantRepository;
+    private UserRepository userRepository;
 
     private static final Logger LOGGER = Logger.getLogger(ReviewService.class.getName());
 
@@ -21,10 +30,33 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
     }
 
-//  http://localhost:9092/api/restaurants/1/reviews
-    public List<Review> getRestaurantReviews(@PathVariable(value = "restaurantId") Long categoryId){
-        LOGGER.info("calling getRestaurantReviews from controller");
-        return reviewRepository.findAll();
+    @Autowired
+    public void setRestaurantRepository(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<Review> getRestaurantReviews(Long restaurantId){
+        LOGGER.info("calling getRestaurantReviews from service");
+        return reviewRepository.findByRestaurantId(restaurantId);
+    }
+
+    public Review createRestaurantReview( Long restaurantId, Long userId, Review reviewObject){
+        LOGGER.info("calling createRestaurantReview from controller");
+        Optional<Restaurant> restaurant = restaurantRepository.findById(restaurantId);
+        Optional<User> user = userRepository.findById(userId);
+        if( restaurant.isPresent() && user.isPresent()){
+            reviewObject.setRestaurant(restaurant.get());
+            reviewObject.setUser(user.get());
+            return reviewRepository.save(reviewObject);
+        } else {
+            throw new InformationNotFoundException("restaurant with id of " + restaurantId + " does not exist");
+        }
     }
 
 }
